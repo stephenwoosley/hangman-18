247b817713f3bcee669beecc23ec9f07d0dce0b7
Stephen Woosley <stephenrwoosley@gmail.com>
2018-03-15 19:30:46 -0400

    game object setup

--- hmjs.js
+++ hmjs.js
@@ -3,24 +3,59 @@
 
 var game = {
   userGuess: "",
-  wordlist: ["Duke", "Carolina", "Clemson", "NC State", "Florida State", "Miami"],
+  computerWord: "",
+  solved: false,
+  wordlist: ["DUKE", "CAROLINA", "CLEMSON", "NC STATE", "FLORIDA STATE", "MIAMI"],
+  computerWordLetters: [],
+  blankArray: [],
   resetGame: function() {
     this.userGuess = "";
   },
+  playGame: function() {
+    this.chooseComputerWord();
+    this.listen();
+  },
   listen: function() {
     document.onkeyup = function(event) {
-      console.log(this.userGuess);
-      console.log(this.wordlist[0]);
       this.userGuess = event.key;
-      console.log(this.userGuess);
+      console.log("User guess: " + this.userGuess);
     }
   },
-  chooseWord: function() {
-    var length = game.wordlist.length;
-    var randomWord = Math.floor(Math.random()*length)+1;
-    console.log(randomWord);
+  chooseComputerWord: function() {
+    var randomNum = Math.floor(Math.random()*this.wordlist.length);
+    console.log("Random number: " + randomNum);
+    this.computerWord = this.wordlist[randomNum];
+    // fill computerWordLetters array with individual letters of word
+    this.computerWordLetters = this.computerWord.split("");
+    console.log("Computer word: " + this.computerWord);
+    console.log("Letters of computer word: " + this.computerWordLetters);
+  },
+  fillBlanks: function(computerWord) {
+    for (let i = 0; i < computerWord.length; i++) {
+      computerWord.push("_");
+    }
+  },
+  updateBlanks: function(userGuess, computerWordLetters, blankArray) {
+    // if user has not won
+    if (!this.solved) {
+      // fill the blanks array with, or output to the page, either a blank or a letter
+      // 1. 
+      for (var i = 0; i < blankArray.length; i++) {
+        // if it's a blank & not a letter already, run the letter check
+        if(blankArray[i] === "_" && userGuess === computerWordLetters[i]) {
+          // replace the current blank with the userGuess
+          blankArray[i] = userGuess;
+        }
+      } // end for loop
+      console.log(blankArray);
+    } // end if (!this.solved)
   }
 }
 
-document.querySelector(".title").onclick = game.chooseWord;
-game.listen();
+// binds the chooseWord function to its game scope as opposed to Window, where it would be called from/bound to otherwise
+//var boundChooseWord = game.chooseComputerWord.bind(game);
+var boundListener = game.listen.bind(game);
+
+//document.querySelector(".title").onclick = boundChooseWord;
+// boundListener();
+game.playGame();
